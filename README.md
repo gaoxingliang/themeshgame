f4ebf4766e934790852fb63531f15db05c8d789a
edward.gao <edward.gao@logicmonitor.com>
2017-11-19 18:29:49 +0800

    provide the build and support for multiple two

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # themeshgame
-the algorithm of the The mesh
+the algorithm of the The mesh game - http://themeshgame.com/
+
+![game](/imgs/game.png?raw=true "the game")
+
+The main logic is:<br>
+eg we got four numbers, [1, 2, 3, 4]<br>
+    let's use the [1, 2] first, <br>
+    then we got 3 numbers. [1, 3, 4] or [3, 3, 4]<br>
+    let's use the [1,3] first, <br>
+    then we got two numbers, [2, 4] or [4, 4]<br>
+    that's the process.<br>
+    
+<br>    
+Now we support those operators:
+1. default
+2. multiply 2 (or reverse divide 2)
+
+#How to build
+call 'gradle fatrjar'<br>
+then 'java -jar build/libs/themeshgame-all-0.1.jar'
--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,108 @@
+import com.google.common.base.Splitter;
+
+import java.io.Console;
+import java.util.Arrays;
+
 /**
- * The main logic:
- * for the four numbers:
- * <p>
- * <p>
+ *
  * Created by edward.gao on 16/11/2017.
  */
 public class Main {
 
 
     /**
+     * a test
      * @param args
      */
     public static void main(String[] args) {
-        int[] numbers = {2, 5, 6, 7, 8};
-        KIND[] kinds = {KIND.MULTIPLY2, KIND.DEFAULT, KIND.DEFAULT, KIND.DEFAULT, KIND.DEFAULT};
-        int target = 1;
-        System.out.println(solve(target, numbers, kinds));
+
+        Console c = System.console();
+        while (true) {
+            c.printf("input the numbers\n");
+            String numbers = c.readLine();
+            if (numbers.isEmpty()) {
+                c.printf("No numbers");
+                continue;
+            }
+            int targetNum = 0;
+            while (true) {
+                c.printf("input the target\n");
+                String target = c.readLine();
+                if (target.isEmpty()) {
+                    c.printf("No target");
+                    continue;
+                }
+                targetNum = Integer.valueOf(target);
+                break;
+            }
+            int [] nums = Splitter.on(",").trimResults().omitEmptyStrings().splitToList(numbers).stream().mapToInt(k -> Integer.valueOf(k)).sorted().toArray();
+            System.out.println("Number are - " + Arrays.asList(nums));
+            c.printf("which numbers has multiple 2? \n");
+            String muls = c.readLine();
+            KIND [] kinds = new KIND[nums.length];
+            int mulsArr [] =new int[0];
+            if (muls.length() > 0) {
+                mulsArr =  Splitter.on(",").trimResults().omitEmptyStrings().splitToList(muls).stream().mapToInt(k -> Integer.valueOf(k)).toArray();
+            }
+            for (int i = 0; i < nums.length; i++) {
+                KIND kind = Main.KIND.DEFAULT;
+                for (int v : mulsArr) {
+                    if (v == nums[i]) {
+                        kind = KIND.MULTIPLY2;
+                        break;
+                    }
+                }
+                kinds[i] = kind;
+            }
+            System.out.println(solve(targetNum, nums, kinds));
+
+        }
+
+
 
     }
 
 
-    public static boolean isTarget(int target, String[] exps, int[] numbers, KIND[] kinds, boolean[] used, int n) {
+    public static boolean isTarget(int target, String[] exps, int[] numbers, KIND[] kinds, int n) {
 
         if (n == 1) {
             return numbers[0] == target;
         }
         for (int i = 0; i < n; i++) {
+
             for (int j = i + 1; j < n; j++) {
+
                 int a, b;
                 String expa, expb;
                 a = numbers[i];
                 b = numbers[j];
-                used[i] = used[j] = true;
                 numbers[j] = numbers[n - 1];
                 expa = exps[i];
                 expb = exps[j];
                 exps[j] = exps[n - 1];
-
+                // record previous kind and result
+                KIND kinda = kinds[i], kindb = kinds[j];
 
                 if (kinds[i] == KIND.DEFAULT && kinds[j] == KIND.DEFAULT) {
                     exps[i] = "(" + expa + "+" + expb + ")";
 
                     numbers[i] = a + b;
-                    if (isTarget(target, exps, numbers, kinds, used, n - 1)) {
+                    kinds[i] = kinds[j] = KIND.DEFAULT;
+
+                    if (isTarget(target, exps, numbers, kinds, n - 1)) {
                         return true;
                     }
 
+                    if (a > b) {
+                        exps[i] = "(" + expa + "-" + expb + ")";
+                        numbers[i] = a - b;
+                    }
+                    else {
+                        exps[i] = "(" + expb + "-" + expa + ")";
+                        numbers[i] = b - a;
+                    }
 
-                    exps[i] = "(" + expa + "-" + expb + ")";
-
-                    numbers[i] = a - b;
-                    if (a > b && isTarget(target, exps, numbers, kinds, used, n - 1)) {
+                    if (isTarget(target, exps, numbers, kinds, n - 1)) {
                         return true;
                     }
 
@@ -60,24 +112,28 @@ public class Main {
                     exps[i] = "(" + expa + "*" + expb + ")";
 
                     numbers[i] = a * b;
-                    if (isTarget(target, exps, numbers, kinds, used, n - 1)) {
+                    kinds[i] = kinds[j] = KIND.DEFAULT;
+                    if (isTarget(target, exps, numbers, kinds, n - 1)) {
                         return true;
                     }
 
                     exps[i] = "(" + expa + "/" + expb + ")";
-
-                    numbers[i] = a / b;
-                    if (a % b == 0 & isTarget(target, exps, numbers, kinds, used, n - 1)) {
-                        return true;
+                    if (b > 0 && a % b == 0) {
+                        numbers[i] = a / b;
+                        if (isTarget(target, exps, numbers, kinds, n - 1)) {
+                            return true;
+                        }
                     }
+
                 }
 
 
                 if (kinds[i] == KIND.MULTIPLY2 && kinds[j] == KIND.DEFAULT) {
-                    exps[i] = "(" + expa + "*" + expb + ")";
+                    exps[i] = "(" + expb + "*" + expa + ")";
 
                     numbers[i] = a * b;
-                    if (isTarget(target, exps, numbers, kinds, used, n - 1)) {
+                    kinds[i] = kinds[j] = KIND.DEFAULT;
+                    if (isTarget(target, exps, numbers, kinds, n - 1)) {
                         return true;
                     }
 
@@ -87,7 +143,7 @@ public class Main {
                     if (a > 0 && b % a == 0) {
 
                         numbers[i] = b / a;
-                        if (isTarget(target, exps, numbers, kinds, used, n - 1)) {
+                        if (isTarget(target, exps, numbers, kinds, n - 1)) {
                             return true;
                         }
                     }
@@ -97,7 +153,8 @@ public class Main {
                 numbers[j] = b;
                 exps[i] = expa;
                 exps[j] = expb;
-                used[i] = used[j] = false;
+                kinds[i] = kinda;
+                kinds[j] = kindb;
             }
         }
         return false;
@@ -106,20 +163,13 @@ public class Main {
 
     public static Result solve(int target, int[] numbers, KIND[] kinds) {
         Result r = new Result();
-        boolean marked[] = new boolean[numbers.length];
 
 
         String[] exps = new String[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
             exps[i] = String.valueOf(numbers[i]);
-            if (i == 0) {
-                marked[0] = true;
-            }
-            else {
-                marked[i] = false;
-            }
         }
-        boolean isTrue = isTarget(target, exps, numbers, kinds, marked, numbers.length);
+        boolean isTrue = isTarget(target, exps, numbers, kinds, numbers.length);
         if (isTrue) {
             r.matched = true;
             r.expression = exps[0];
@@ -128,9 +178,13 @@ public class Main {
     }
 
 
+    /**
+     * the number kind
+     *
+     */
     enum KIND {
-        DEFAULT,
-        MULTIPLY2
+        DEFAULT,  // default ,only + - could be used
+        MULTIPLY2 // multiple 2, divide 2
     }
 
 
